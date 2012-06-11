8f9e3d6684c5cb79798256b76cb67b984acb1d1b
Paul Ennemoser <tick@federrot.at>
2012-06-12 01:38:26 +0200

    Improved Natural Docs usage by requiring the tool to be in path. Improved Readme.

--- build/make-doc-win.bat
+++ build/make-doc-win.bat
@@ -1,8 +1,7 @@
 @echo Generating natural doc from source code..
 setlocal 
 
-cd ..\tools\generic\NaturalDocs
-mkdir ..\..\..\doc\natural
-mkdir ..\..\..\project\doc\natural
+mkdir ..\doc\natural
+mkdir ..\project\doc\natural
 
-NaturalDocs.bat -i "../../../include"  -i "../../../src" -o HTML "../../../doc/natural" -p "../../../project/doc/natural" 
+NaturalDocs.bat -i "../include"  -i "../src" -o HTML "../doc/natural" -p "../project/doc/natural" 
